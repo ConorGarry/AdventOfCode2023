3ac6f7f0d775eb26cd7261728931507b7ca0842f
Conor Garry <conor.garry@marinosoftware.com>
2023-12-04 22:21:28 +0000

    Complete Day04

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -1,55 +1,41 @@
-data class Card(
-    val id: Int,
-    val winning: List<Int>,
-    val entries: List<Int>,
-)
+data class Card(val id: Int, val winning: List<Int>, val entries: List<Int>)
 
 fun getCards(input: List<String>): List<Card> =
     input.map {
         val splits = it.split(": ")
-        val lists = splits[1].split(" | ")
+        val (winners, entries) = splits[1].split(" | ")
+            .map { it.split(" ").filter(String::isNotEmpty).map(String::toInt) }
         Card(
             splits[0].substringAfterLast(" ").toInt(),
-            lists[0].split(" ").filter(String::isNotEmpty).map(String::toInt),
-            lists[1].split(" ").filter(String::isNotEmpty).map(String::toInt),
+            winners,
+            entries,
         )
     }
 
-fun Card.numWinners(): Int =
-    with(entries.intersect(winning.toSet()).size) {
-        if (this > 0) (1..<this).fold(1) { acc, _ -> acc * 2 } else 0
-    }
+fun Card.numWinners(): Int = entries.count { it in winning }
 
 class Day04 {
-    fun part1(input: List<String>): Int { // 19855
-        val cards = getCards(input)
-        return cards.sumOf(Card::numWinners)
-    }
+    fun part1(input: List<String>): Int =
+        getCards(input).sumOf(Card::numWinners)
 
-    fun part2(input: List<String>): Long {
-        val cards: List<Card> = getCards(input)
-        val cartCounts = LongArray(cards.size) { 1 }
-        println("cartCounts: ${cartCounts.size}")
+    fun part2(input: List<String>): Int {
+        val cards = getCards(input)
+        val counts = IntArray(cards.size) { 1 }
         for (i in input.indices) {
             val card = cards[i]
-            for (j in i + 1..i + card.numWinners()) {
-
-                println("i: $i, j: $j, numwinner: ${card.numWinners()}")
-                /*if (j == card.numWinners()) continue*/
-                if (j >= cartCounts.size) {
-                    continue
-                }
-                cartCounts[j] += cartCounts[i]
+            val j = card.numWinners()
+            for (n in i + 1..<i + j + 1) {
+                counts[n] += counts[i]
             }
         }
-        return cartCounts.sum()
+        return counts.sum()
     }
 }
 
 fun main() {
     val input = readInput("Day04")
     with(Day04()) {
-        /*part1(input).println()*/
+        part1(input).println()
         part2(input).println()
     }
 }
