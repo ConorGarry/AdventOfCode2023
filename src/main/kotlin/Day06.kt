c7ad06b274e86c910f78b25a966b426729ab5e75
Conor Garry <conorgarry@gmail.com>
2023-12-06 07:17:00 +0000

    Complete Day06

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -2,40 +2,33 @@ class Day06 {
 
     fun numsFromString(s: String) = "\\d+".toRegex().findAll(s).map { it.value.toInt() }.toList()
 
-    fun parseInput(input: List<String>) = input.let { (times, distances) ->
+    fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> = input.let { (times, distances) ->
         numsFromString(times) to (numsFromString(distances))
     }
 
-    fun part1(input: List<String>): Int {
-        val (times, distances) = parseInput(input)
-        var sum = 1
-        times.indices.forEach {
-            var result = 0
-            val total = times[it]
-            val distance = distances[it]
-            (1..total).forEach { pressMs ->
-                val dist = (total - pressMs) * pressMs
-                if (dist > distance) {
-                    result++
+    fun part1(input: List<String>): Int =
+        parseInput(input).first.zipWithNext().fold(1) { acc, (time, distance) ->
+            var res = 0
+            (1..time).forEach { press ->
+                val d = (time - press) * press
+                if (d > distance) {
+                    res++
                 }
             }
-            sum *= result
+            acc * res
         }
-        return sum
-    }
 
     fun part2(input: List<String>): Int {
-        val (times, distances) = parseInput(input)
-        val time = times.joinToString("").toLong()
-        val distance = distances.joinToString("").toLong()
-        var result = 0
+        val (time, distance) =
+            parseInput(input).toList().map { it.joinToString("").toLong() }
+        var res = 0
         (1..time).forEach { pressMs ->
             val dist = (time - pressMs) * pressMs
             if (dist > distance) {
-                result++
+                res++
             }
         }
-        return input.size
+        return res
     }
 }
 
