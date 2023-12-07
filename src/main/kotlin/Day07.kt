ebd1315bbf86d860468083de4d425c1b2e714051
Conor Garry <conorgarry@gmail.com>
2023-12-07 18:32:13 +0000

    WIP Day07, pt2 done

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -1,80 +1,133 @@
-import Hand.Type.*
+import Type.*
 
-private val handComparator by lazy {
-    val charValues = mapOf(
-        'A' to 12, 'K' to 11, 'Q' to 10,
-        'J' to 9, 'T' to 8, '9' to 7,
-        '8' to 6, '7' to 5, '6' to 4,
-        '5' to 3, '4' to 2, '3' to 1, '2' to 0
-    )
+val charValues = mapOf(
+    'A' to 14, 'K' to 13, 'Q' to 12,
+    'J' to 11, 'T' to 10, '9' to 9,
+    '8' to 8, '7' to 7, '6' to 6,
+    '5' to 5, '4' to 4, '3' to 3, '2' to 2
+)
 
-    Comparator<Hand> { h1, h2 ->
-        when (val comp = h1.type.value.compareTo(h2.type.value)) {
+fun comparatorFor(type: (List<Char>) -> Type, values: Map<Char, Int>): Comparator<Hand> =
+    Comparator { h1, h2 ->
+        when (val comp = type(h1.cards).value.compareTo(type(h2.cards).value)) {
             0 -> {
-                val comparator: (Char) -> Int = { charValues.getOrDefault(it, 0) }
-                h1.cards.zip(h2.cards).map { (l, r) -> comparator(l).compareTo(comparator(r)) }.first { it != 0 }
+                val comparator: (Char) -> Int = { values.getOrDefault(it, 0) }
+                h1.cards.zip(h2.cards).map { (l, r) ->
+                    comparator(l).compareTo(comparator(r))
+                }.first { it != 0 }
             }
 
             else -> comp
         }
     }
+
+enum class Type(val value: Int) {
+    FiveOfAKind(7),
+    FourOfAKind(6),
+    FullHouse(5),
+    ThreeOfAKind(4),
+    TwoPair(3),
+    OnePair(2),
+    HighCard(1),
 }
 
-data class Hand(
-    val cards: List<Char>,
-    val bid: Int,
-) {
+fun List<Char>.unique() = distinct().size
 
-    enum class Type(val value: Int) {
-        FiveOfAKind(7),
-        FourOfAKind(6),
-        FullHouse(5),
-        ThreeOfAKind(4),
-        TwoPair(3),
-        OnePair(2),
-        HighCard(1),
+fun typeForCards(cards: List<Char>): Type =
+    when (buildCharMap(cards).values.max()) {
+        5 -> FiveOfAKind
+        4 -> FourOfAKind
+        3 -> if (cards.unique() == 2) FullHouse else ThreeOfAKind
+        2 -> if (cards.unique() == 3) TwoPair else OnePair
+        else -> HighCard
     }
 
-    val type: Type
-        get() {
-            val charMap = buildMap<Char, Int> {
-                for (char in cards) {
-                    this[char] = getOrDefault(char, 0) + 1
+fun typeForCardsWithJoker(cards: List<Char>): Type {
+    val map = buildCharMap(cards)
+    fun Map<Char, Int>.anyJokerAvailable() = containsKey('J') || (this['J'] != null)
+    fun Map<Char, Int>.availableJokers() = this['J'].takeIf { it != null } ?: 0
+    return when (map.values.max()) {
+        5 -> FiveOfAKind
+        4 ->
+            if (map['J'] == 4) {
+                FiveOfAKind
+            } else {
+                if (map.anyJokerAvailable()) FiveOfAKind else FourOfAKind
+            }
+
+        3 ->
+            if (map['J'] == 3) {
+                if (cards.unique() == 2) FiveOfAKind else FourOfAKind
+            } else {
+                val jokers = map.availableJokers()
+                if (jokers > 0) {
+                    if (jokers == 2) FiveOfAKind else FourOfAKind
+                } else {
+                    if (cards.unique() == 2) FullHouse else ThreeOfAKind
                 }
             }
-            /*println("charMap: $charMap for $cards")*/
-            return when (charMap.values.max()) {
-                5 -> FiveOfAKind
-                4 -> FourOfAKind
-                3 -> if (cards.toSet().size == 2) FullHouse else ThreeOfAKind
-                2 -> if (cards.toSet().size == 3) TwoPair else OnePair
-                else -> HighCard
+
+        2 ->
+            if (map['J'] == 2) {
+                when (cards.unique()) {
+                    4 -> ThreeOfAKind
+                    3 -> FourOfAKind
+                    else -> OnePair
+                }
+            } else {
+                when (map.availableJokers()) {
+                    3 -> FiveOfAKind
+                    2 -> FourOfAKind
+                    1 -> if (cards.unique() == 3) FullHouse else ThreeOfAKind
+                    else -> {
+                        if (cards.unique() == 3) TwoPair else OnePair
+                    }
+                }
             }
-        }
+
+        else -> if (map['J'] == 1 && map.anyJokerAvailable()) OnePair else HighCard
+    }
 }
 
+private fun buildCharMap(cards: List<Char>): Map<Char, Int> =
+    buildMap<Char, Int> {
+        for (char in cards) {
+            this[char] = getOrDefault(char, 0) + 1
+        }
+    }
+
+data class Hand(val cards: List<Char>, val bid: Int)
+
 class Day07 {
-    fun part1(input: List<String>): Int {
-        val hands = input.map {
-            it.split(" ").let { (cards, bid) ->
+
+    val parse: (List<String>) -> List<Hand> = {
+        it.map { line ->
+            line.split(" ").let { (cards, bid) ->
                 Hand(cards.toList(), bid.toInt())
             }
         }
-        return hands.sortedWith(handComparator)
+    }
+
+    fun part1(input: List<String>): Int =
+        parse(input).sortedWith(comparatorFor(::typeForCards, charValues))
             .foldIndexed(0) { i, sum, (_, bid) ->
                 sum + bid * (i + 1)
             }
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int =
+        parse(input).sortedWith(
+            comparatorFor(
+                ::typeForCardsWithJoker,
+                (charValues as MutableMap<Char, Int>).apply { this['J'] = 0 })
+        ).foldIndexed(0) { i, sum, (_, bid) ->
+            sum + bid * (i + 1)
+        }
 }
 
 fun main() {
     val input = readInput("Day07")
     with(Day07()) {
-        part1(input).println() // 241344943
-        /*part2(input).println()*/
+        part1(input).println()
+        part2(input).println()
     }
 }
