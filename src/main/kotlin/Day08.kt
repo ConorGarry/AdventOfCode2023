5e527ac4c82c7b7199fafd7623d2cc79a895de7a
Conor Garry <conorgarry@gmail.com>
2023-12-08 12:17:24 +0000

    WIP Day08, pt2 done

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -1,7 +1,6 @@
 import kotlin.math.abs
 
 data class NodeMap(val directions: String, val nodes: Map<String, Pair<String, String>>)
-data class Traversal(var current: String, var sum: Int = 0)
 
 fun lcm(a: Long, b: Long): Long {
     fun gcd(a: Long, b: Long): Long = if (b == 0L) abs(a) else gcd(b, a % b)
@@ -18,42 +17,42 @@ class Day08 {
             directions = input.first(),
             nodes = buildMap {
                 input.slice(2..<input.size).forEach {
-                    val (key, value1, value2) =
-                        """(\w+) = \((\w+), (\w+)\)""".toRegex().matchEntire(it)!!.destructured
-                    put(key, value1 to value2)
+                    val (key, left, right) =
+                        """(\w+) = \((\w+), (\w+)\)"""
+                            .toRegex().matchEntire(it)!!.destructured
+                    put(key, left to right)
                 }
             }
         )
 
-    fun part1(input: List<String>): Int {
-        val nodeMap = parse(input)
-        var current = "AAA"
-        var sum = 0
+    fun countSteps(
+        start: String,
+        nodeMap: NodeMap,
+        until: (String) -> Boolean
+    ): Long {
+        var sum = 0L
         var i = 0
-        while (current != "ZZZ") {
+        var current = start
+        while (until(current)) {
             val idx = i++ % nodeMap.directions.length
             val direction = nodeMap.directions[idx]
-            current = nodeMap.nodes[current]!!.let { (l, r) -> if (direction == 'L') l else r }
+            current = nodeMap.nodes[current]!!
+                .let { (l, r) -> if (direction == 'L') l else r }
             sum++
         }
         return sum
     }
 
+    fun part1(input: List<String>): Long =
+        countSteps("AAA", parse(input), until = { it != "ZZZ" })
+
     fun part2(input: List<String>): Long =
         parse(input).let { nodeMap ->
             lcmOfList(
-                nodeMap.nodes.keys.filter { it.endsWith('A') }
-                    .map { Traversal(it) }.map {
-                        var i = 0
-                        while (!it.current.endsWith('Z')) {
-                            val idx = i++ % nodeMap.directions.length
-                            val direction = nodeMap.directions[idx]
-                            it.current = nodeMap.nodes[it.current]!!.let { (l, r) ->
-                                if (direction == 'L') l else r
-                            }
-                            it.sum++
-                        }
-                        it.sum.toLong()
+                nodeMap.nodes.keys
+                    .filter { it.endsWith('A') }
+                    .map { start ->
+                        countSteps(start, nodeMap, until = { !it.endsWith('Z') })
                     }
             )
         }
