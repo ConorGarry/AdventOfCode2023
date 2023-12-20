5f268093f3cea6ef0080d38196955d444dc54561
Conor Garry <conorgarry@gmail.com>
2023-12-20 07:11:39 +0000

    Move List splitter from Day 13 to Utils

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -1,13 +1,4 @@
-import kotlin.math.min
-
 class Day13 {
-
-    fun <T> List<T>.split(predicate: (T) -> Boolean): List<List<T>> =
-        fold(mutableListOf(mutableListOf<T>())) { acc, t ->
-            if (predicate(t)) acc.add(mutableListOf())
-            else acc.last().add(t)
-            acc
-        }.filterNot { it.isEmpty() }
 
     private fun List<String>.reflection() =
         zipWithNext { s, other -> s == other }
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -32,3 +32,10 @@ enum class Direction(val coords: Pair<Int, Int>) {
     Right(0 to 1),
     None(0 to 0)
 }
+
+fun <T> List<T>.split(predicate: (T) -> Boolean): List<List<T>> =
+    fold(mutableListOf(mutableListOf<T>())) { acc, t ->
+        if (predicate(t)) acc.add(mutableListOf())
+        else acc.last().add(t)
+        acc
+    }.filterNot { it.isEmpty() }
